b35cf2da75bb11ca9367dfd04c3714d8cbebe837
vagrant <vagrant@localhost.localdomain>
2016-02-16 16:24:47 +0000

    test

--- builtin/providers/rightscale/resource_rightscale_cloud.go
+++ builtin/providers/rightscale/resource_rightscale_cloud.go
@@ -2,6 +2,7 @@ package rightscale
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/rightscale/rsc/cm15"
@@ -10,7 +11,10 @@ import (
 
 func resourceRightScaleCloud() *schema.Resource {
 	return &schema.Resource{
-		Read: resourceRightScaleCloudRead,
+		Create: resourceRightScaleCloudCreate,
+		Read:   resourceRightScaleCloudRead,
+		Update: resourceRightScaleCloudUpdate,
+		Delete: resourceRightScaleCloudDelete,
 
 		Schema: map[string]*schema.Schema{
 			"cloud_type": &schema.Schema{
@@ -20,7 +24,7 @@ func resourceRightScaleCloud() *schema.Resource {
 
 			"name": &schema.Schema{
 				Type:     schema.TypeString,
-				Optional: true,
+				Required: true,
 			},
 
 			"description": &schema.Schema{
@@ -32,18 +36,42 @@ func resourceRightScaleCloud() *schema.Resource {
 }
 
 func resourceRightScaleCloudRead(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[INFO] resourceRightScaleCloudRead")
+
 	client := meta.(*cm15.API)
 
 	l := client.CloudLocator("/api/clouds")
-	cloud, err := l.Show(rsapi.APIParams{"id": d.Id()})
+	clouds, err := l.Index(rsapi.APIParams{"filter": "name==" + d.Get("name").(string)})
 
 	if err != nil {
 		return fmt.Errorf("Failed to read RightScale cloud: %s", err)
 	}
 
+	cloud := clouds[0]
+
+	d.SetId(cloud.Name)
+
 	d.Set("name", cloud.Name)
 	d.Set("description", cloud.Description)
 	d.Set("cloud_type", cloud.CloudType)
+
+	return nil
+}
+
+func resourceRightScaleCloudCreate(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[INFO] resourceRightScaleCloudCreate")
+
+	return resourceRightScaleCloudRead(d, meta)
+}
+
+func resourceRightScaleCloudUpdate(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[INFO] resourceRightScaleCloudUpdate")
+
+	return resourceRightScaleCloudRead(d, meta)
+}
+
+func resourceRightScaleCloudDelete(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[INFO] resourceRightScaleCloudDelete")
 
 	return nil
 }
--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "rightscale" {
-  refreshToekn = ""
-  accountID = ""
+  cloud_name = "EC2 eu-west-1"
 }
 
-resource "rightscale_cloud" "eui" {
-  id = "2"
+resource "rightscale_network" "terraform.test" {
+  cidr_block = "192.168.0.0/24" 
 }
